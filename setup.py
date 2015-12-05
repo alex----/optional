3e7536a30a7d21e78db84234e90c3404ada3eefc
alex---- <alex----@users.noreply.github.com>
2015-12-04 21:20:56 -0800

    upping version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 
 setup(name='optional',
-      version='0.0.2',
+      version='0.0.3',
       description='Optional Class for Python2/3',
       author='Alex',
       url='https://github.com/alex----/optional',
